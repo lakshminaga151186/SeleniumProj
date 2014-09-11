b5f362578907cfaf691281acab8ca9014d1201ed
chinu <chinu@chinu-PC>
2014-09-12 00:55:16 +0530

    correcting

--- SeleniumAutomation/src/corejava/Gmail_Login.java
+++ SeleniumAutomation/src/corejava/Gmail_Login.java
@@ -1,8 +1,10 @@
 package corejava;
 
+import webdriver.AllBrowsers;
+
 import com.thoughtworks.selenium.DefaultSelenium;
 
-public class Gmail_Login extends allbrowsers {
+public class Gmail_Login extends AllBrowsers {
  
 	public static void main(String[] arg){
 		DefaultSelenium log = new DefaultSelenium("localhost", 4444, "*firefox", "https://mail.google.com");
